11238538466eaf086c7bc36cbe865dfe6e3d81f2
José <jose.n.widder@gmail.com>
2025-10-24 14:26:22 -0300

    Seguridad añadida con helmet y express-rate-limiting

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import routes from "./src/routes/index.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./src/config/swagger.js";
 import config from "./src/config/env.js";
 
 const app = express();
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
+app.use(helmet());
+app.use(limiter);
 app.use(cors({ origin: config.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
